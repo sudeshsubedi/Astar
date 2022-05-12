9e2c35faf11baa9e04263616471190134eba1482
sudeshsubedi <programmedevil13@gmail.com>
2022-05-12 12:22:33 +0545

    changed height/weight to make the grid completely displayed in terminal

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define HEIGHT 41 // in the form of 2 * n - 1
-#define WIDTH 41 // in the form of 2 * n - 1
+#define HEIGHT 35 // in the form of 2 * n - 1
+#define WIDTH 35 // in the form of 2 * n - 1
 
 
 #define BG_WHITE "\033[47m"
